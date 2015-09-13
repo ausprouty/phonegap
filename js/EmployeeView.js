ad082d60c96f7d92b5defab52fa2350f317f27ad
Bob <bob@hereslife.com>
2015-09-13 16:46:11 +1000

    save

--- js/EmployeeView.js
+++ js/EmployeeView.js
@@ -1,4 +1,5 @@
 var EmployeeView = function(employee) {
+var EmployeeView = function(employee) {
 
     this.initialize = function() {
         this.el = $('<div/>');
@@ -38,6 +39,34 @@ var EmployeeView = function(employee) {
         phoneNumbers[1] = new ContactField('mobile', app.currentEmployee.cellPhone, true); // preferred number
         contact.phoneNumbers = phoneNumbers;
         contact.save();
+        return false;
+    };
+
+    this.initialize();
+
+}
+
+EmployeeView.template = Handlebars.compile($("#employee-tpl").html());
+    this.initialize = function() {
+        this.el = $('<div/>');
+        this.el.on('click', '.add-location-btn', this.addLocation);
+    };
+
+    this.render = function() {
+        this.el.html(EmployeeView.template(employee));
+        return this;
+    };
+
+    this.addLocation = function(event) {
+        event.preventDefault();
+        console.log('addLocation');
+        navigator.geolocation.getCurrentPosition(
+            function(position) {
+                $('.location', this.el).html(position.coords.latitude + ',' +position.coords.longitude);
+            },
+            function() {
+                alert('Error getting location');
+            });
         return false;
     };
 
